8be0878c46606337079fc5a9d88a99f2dfd64d6b
ReizenCreations <reizencreations.eth@gmail.com>
2025-09-24 15:42:30 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
             <img 
               src={headerLogoUrl} 
               alt="Reizen Creations Logo" 
-              className="w-15 h-auto"
+              className="w-20 h-auto"
             />
           </div>
         </div>
